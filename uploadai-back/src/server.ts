b12e1b6501731cdc59c2a9907fed2b2f1761d900
jovimoura <joaovictors.mouraa@gmail.com>
2023-09-14 12:22:11 -0300

    back done

---
+++ uploadai-back/src/server.ts
@@ -0,0 +1,25 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAiCompletionRoute);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running");
+  });
